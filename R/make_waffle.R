f0a7b871afe574a395f0c3a01e68d71087325ce3
Gabriel J. Odom <gjo15@med.miami.edu>
2018-06-07 18:16:53 -0400

    Changing argument fills from is.null() to missing() will be hopefully more robust. closes #1.

--- R/make_waffle.R
+++ R/make_waffle.R
@@ -1,23 +1,28 @@
 #' Make a Waffle Plot
 #'
 #' @param vec a numeric vector of strictly non-negative values
-#' @param title the title of the plot
-#' @param colours a character vector of colours the same length as \code{vec}
+#' @param title the title of the plot. Defaults to the name of \code{vec}.
+#' @param colors a character vector of colors the same length as \code{vec}.
+#'    Defaults to colors from \code{\link[grDevices]{rainbow}}.
 #'
-#' @return Nothing. A waffle plot is displayed
+#' @return Nothing. A waffle plot is displayed.
 #' @export
 #'
 #' @importFrom waffle waffle
 #' @importFrom grDevices rainbow
 #'
 #' @examples
-#'   MakeWaffle(vec = runif(5), title = "test")
-MakeWaffle <- function(vec, title, colours = NULL){
-
-  if(is.null(colours)){
+#'   MakeWaffle(vec = runif(5), title = "Waffle plot of random proportions")
+MakeWaffle <- function(vec, title, colors){
+  
+  if(missing(colors)){
     colors <- rainbow(length(vec))
   }
-
+  
+  if(missing(title)){
+    title <- paste("Waffle plot of", deparse(substitute(vec)))
+  }
+  
   waffle(round(vec / sum(vec) * 100),
          rows = 10,
          size = 0.5,
@@ -25,7 +30,7 @@ MakeWaffle <- function(vec, title, colours = NULL){
          title = title,
          colors = colors,
          reverse = TRUE)
-
+  
 }
 
 
